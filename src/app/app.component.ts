cbed3f2fdb192b46fd63bc3718f86c2256d5acf1
Victor Oliveira <victor.oliveira@avanade.com>
2024-01-24 10:17:54 -0300

    fix: add carrousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare let LiquidCorp: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'miniapp';
+
+  ngOnInit() {
+    const options = {
+      targetSelector: "#myCarousel",
+      config: {
+        centeredSlides: false,
+        slidesPerView: 2,
+        initialSlide: 1,
+        loop: false,
+        autoplay: { delay: 1000 },
+      },
+    };
+    const service = LiquidCorp.BradCarouselService.getInstance(options);
+  }
 }
