25d878b914af4ebaebda8b6acc0e0b7984c0bb17
Victor Oliveira <victor.oliveira@avanade.com>
2024-01-25 13:25:35 -0300

    fix: melhoria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,6 @@ const routes: Routes = [
 
 @NgModule({
   exports: [RouterModule],
-  imports: [RouterModule.forRoot(routes, { useHash: false })],
+  imports: [RouterModule.forRoot(routes, { useHash: false, enableTracing: true })],
 })
 export class AppRoutingModule { }
