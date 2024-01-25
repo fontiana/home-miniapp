5c9c6f7ec40446c885e459fae69b9c05beda3e47
Victor Oliveira <victor.oliveira@avanade.com>
2024-01-25 12:54:21 -0300

    teste

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { TestComponent } from "./test/test.component";
  
 const routes: Routes = [
   { path: '', loadChildren: () => import('./modules/home.module').then(m => m.HomeModule), canActivate: [] },
   { path: 'teste', loadChildren: () => import('./modules/home.module').then(m => m.HomeModule), canActivate: [] },
+  { path: 'teste2', component: TestComponent }, 
 ];
  
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
