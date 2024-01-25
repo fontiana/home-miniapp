000a8ee846a30e62354da6b8c6699cb10d4d724d
Victor Oliveira <victor.oliveira@avanade.com>
2024-01-25 12:29:06 -0300

    fix: correcao teste

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from "@angular/router";
  
 const routes: Routes = [
   { path: '', loadChildren: () => import('./modules/home.module').then(m => m.HomeModule), canActivate: [] },
+  { path: 'teste', loadChildren: () => import('./modules/home.module').then(m => m.HomeModule), canActivate: [] },
 ];
  
 @NgModule({
   exports: [RouterModule],
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes, { useHash: false })],
 })
 export class AppRoutingModule {}
  
