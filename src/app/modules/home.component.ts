c9d53cec24579aa8bceacdfc453433ecc37259be
Marcelo Bueno <marbue@ig.com.br>
2024-01-29 12:00:28 -0300

    fix: teste

--- src/app/modules/home.component.ts
+++ src/app/modules/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+
+declare let LiquidCorp: any;
  
 @Component({
   selector: "app-home",
@@ -6,7 +8,38 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: [],
 })
 export class HomeComponent implements OnInit { 
-  ngOnInit(): void {}
+  overlayService: any;
+
+  ngOnInit() {
+    const options = {
+      targetSelector: "#myCarousel",
+      config: {
+        centeredSlides: false,
+        slidesPerView: 2,
+        initialSlide: 1,
+        loop: false,
+        autoplay: { delay: 1000 },
+      },
+    };
+    const service = LiquidCorp.BradCarouselService.getInstance(options);
+
+    this.overlayService = LiquidCorp.BradOverlayService.getInstance({
+      id: "myCanvas",
+      color: "brad-bg-overlay-40"
+    });
+  }
+
+  open(target: string) {
+    this.overlayService.open(target);
+  }
+
+  updateTarget(target: string) {
+    this.overlayService.updateTarget(target);
+  }
+
+  close() {
+    this.overlayService.close();
+  }
 
 }
  
