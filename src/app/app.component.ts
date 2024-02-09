c89d55babd71d8f5f710ff2ec7aaffb8a8289d0e
abner cleim albino <abner.cleim.albino@accenture.com>
2024-02-09 15:26:35 -0300

    fix: adjust for prevent double touch move

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ declare let LiquidCorp: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'miniapp';
@@ -13,7 +13,7 @@ export class AppComponent {
 
   ngOnInit() {
     const options = {
-      targetSelector: "#myCarousel",
+      targetSelector: '#myCarousel',
       config: {
         centeredSlides: false,
         slidesPerView: 2,
@@ -25,9 +25,26 @@ export class AppComponent {
     const service = LiquidCorp.BradCarouselService.getInstance(options);
 
     this.overlayService = LiquidCorp.BradOverlayService.getInstance({
-      id: "myCanvas",
-      color: "brad-bg-overlay-40"
+      id: 'myCanvas',
+      color: 'brad-bg-overlay-40',
     });
+
+    document.addEventListener(
+      'touchmove',
+      (event) => {
+        if (this.isTouchEvent(event)) {
+          const touchEvent = event as TouchEvent;
+          if (touchEvent.touches.length > 1) {
+            event.preventDefault();
+          }
+        }
+      },
+      { passive: false }
+    );
+  }
+
+  private isTouchEvent(event: any): event is TouchEvent {
+    return 'TouchEvent' in window && event instanceof TouchEvent;
   }
 
   open(target: string) {
