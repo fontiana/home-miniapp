4053802f1df7cd89a73a47ab1857ecc6d8f5b301
Victor Oliveira <victor.oliveira@avanade.com>
2024-02-20 16:36:45 -0300

    fix: teste

--- src/app/modules/home.component.ts
+++ src/app/modules/home.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from "@angular/core";
- 
+
+declare let LiquidCorp: any;
+
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: [],
 })
-export class HomeComponent implements OnInit { 
-  ngOnInit(): void {}
-
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    const targetSelector = "#drag1"; // ou .classe
+    const options = { targetSelector };
+    const service = LiquidCorp.BradDragAndDropService.getInstance(options);
+  }
 }
- 
