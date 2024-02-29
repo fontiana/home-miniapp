49bae0e874d442f9ca4ac2a89a6d64deb4e94246
abner cleim albino <abner.cleim.albino@accenture.com>
2024-02-28 22:27:45 -0300

    feat: add declarations for any new component or element and routes

--- src/app/modules/home.module.ts
+++ src/app/modules/home.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
- 
- 
+import { DragAndDrop2Component } from './drag-and-drop2/drag-and-drop2.component';
+import { PersonalizarComponent } from './personalizar/personalizar.component';
+import { A11yPipeButtonLabel } from './personalizar/a11y-button-label.pipe';
+import { IconComponent } from './personalizar/atom-icon/atom-icon.component';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'drag-and-drop', component: DragAndDrop2Component },
+  { path: 'personalizar', component: PersonalizarComponent },
 ];
- 
+
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    DragAndDrop2Component,
+    PersonalizarComponent,
+    A11yPipeButtonLabel,
+    IconComponent,
   ],
-  imports: [
-    RouterModule.forChild(routes)
-  ]
+  imports: [RouterModule.forChild(routes), CommonModule],
 })
-export class HomeModule { }
+export class HomeModule {}
