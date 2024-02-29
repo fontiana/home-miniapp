66e0312c4d0e30a1ad15fdf3b2d7a50216f7a2d8
abner cleim albino <abner.cleim.albino@accenture.com>
2024-02-28 22:41:56 -0300

    feat: add new lazy routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,41 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { TestComponent } from "./test/test.component";
-import { PagenotfoundComponent } from "./pagenotfound/pagenotfound.component";
-import { PopoverStepperComponent } from "./components/popover-stepper/popover-stepper/popover-stepper.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { PopoverStepperComponent } from './components/popover-stepper/popover-stepper/popover-stepper.component';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./modules/home.module').then(m => m.HomeModule), canActivate: [] },
-  { path: 'teste', loadChildren: () => import('./modules/home.module').then(m => m.HomeModule), canActivate: [] },
-  { path: 'popover', component: PopoverStepperComponent }
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/home.module').then((m) => m.HomeModule),
+    canActivate: [],
+  },
+  {
+    path: 'teste',
+    loadChildren: () =>
+      import('./modules/home.module').then((m) => m.HomeModule),
+    canActivate: [],
+  },
+  {
+    path: 'drag-and-drop',
+    loadChildren: () =>
+      import('./modules/home.module').then((m) => m.HomeModule),
+    canActivate: [],
+  },
+  {
+    path: 'personalizar',
+    loadChildren: () =>
+      import('./modules/home.module').then((m) => m.HomeModule),
+    canActivate: [],
+  },
+  { path: 'popover', component: PopoverStepperComponent },
 ];
 
 @NgModule({
   exports: [RouterModule],
-  imports: [RouterModule.forRoot(routes, { useHash: true, enableTracing: true })],
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true, enableTracing: true }),
+  ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
