b4671e6e09a7853d6467e2fc84af6129380ec9a2
Victor Oliveira <victor.oliveira@avanade.com>
2024-03-27 11:41:29 -0300

    fix: melhoria

--- src/app/modules/personalizar/personalizar.component.ts
+++ src/app/modules/personalizar/personalizar.component.ts
@@ -55,45 +55,6 @@ export class PersonalizarComponent implements AfterViewInit, OnInit {
 
   constructor(public appService: AppService) { }
 
-  overlayService: any;
-  popoverService: any
-
-  list = [
-    {
-      id: "1",
-      text: "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices venenatis diam a commodo. Maecenas nulla arcu, auctor aliquam mauris non, vehicula eleifend nisl.",
-      target: "target-345",
-    },
-    {
-      id: "2",
-      text: "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices venenatis diam a commodo. Maecenas nulla arcu, auctor aliquam mauris non, vehicula eleifend nisl.",
-      target: "target-2",
-    },
-    {
-      id: "3",
-      text: "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices venenatis diam a commodo. Maecenas nulla arcu, auctor aliquam mauris non, vehicula eleifend nisl.",
-      target: "target-3",
-    },
-  ];
-
-  closeTutorial() {
-    this.overlayService.close();
-    this.popoverService.close();
-  }
-  openTutorial(idTarget: any) {
-    this.popoverService.open();
-    this.popoverService.resetStepper(idTarget);
-    this.overlayService.open(idTarget);
-  }
-  prevTutorial() {
-    this.popoverService.prevStepper();
-    this.overlayService.updateTarget(this.popoverService.stepper.currentItem.target);
-  }
-  nextTutorial() {
-    this.popoverService.nextStepper();
-    this.overlayService.updateTarget(this.popoverService.stepper.currentItem.target);
-  }
-
   public changedLists(): void {
     console.info('[personalizar.component.ts]', '(changedLists)');
     this.appService.addLog({
@@ -316,22 +277,6 @@ export class PersonalizarComponent implements AfterViewInit, OnInit {
 
   ngAfterViewInit(): void {
     const targetSelector = '#drag1'; // ou .classe
-
-
-    this.popoverService = LiquidCorp.BradPopoverService.getInstance({
-      id: "popover-151",
-      idTarget: "target-345",
-      list: this.list,
-      currentItem: this.list[0],
-      direction: "bottom",
-      idOverlay: "myCanvas",
-    });
-
-    this.overlayService = LiquidCorp.BradOverlayService.getInstance({
-      id: "myCanvas",
-      color: 'brad-bg-overlay-60'
-    });
-    console.log(this.popoverService)
 
     console.info('[personalizar.component.ts]', '(ngAfterViewInit)');
 
