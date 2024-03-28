cbe9f6dbefb06cd467ff62876383ebc2a77ad777
Victor Oliveira <victor.oliveira@avanade.com>
2024-03-28 12:08:35 -0300

    fix: teste

--- src/app/modules/popover-stepper/popover-stepper.component.ts
+++ src/app/modules/popover-stepper/popover-stepper.component.ts
@@ -1,97 +1,64 @@
-import { AfterViewInit, Component } from '@angular/core';
-declare let LiquidCorp: any;
-
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare let LiquidCorp:any;
 @Component({
   selector: 'app-popover-stepper',
   templateUrl: './popover-stepper.component.html',
-  styleUrls: ['./popover-stepper.component.css'],
+  styleUrls: ['./popover-stepper.component.css']
 })
-export class PopoverComponent implements AfterViewInit {
+export class PopoverComponent implements AfterViewInit{
   overlayService: any;
-  popoverService: any;
-  selectedOption: string = '1';
+  popoverService:any
 
   ngAfterViewInit() {
     this.popoverService = LiquidCorp.BradPopoverService.getInstance({
-      id: 'popover-151',
-      idTarget: 'target-345',
+      id: "popover-151",
+      idTarget: "target-345",
       list: this.list,
       currentItem: this.list[0],
-      direction: 'bottom',
+      direction: "bottom",
     });
 
     this.overlayService = LiquidCorp.BradOverlayService.getInstance({
-      id: 'myCanvas',
-      color: 'brad-bg-overlay-60',
+      id: "myCanvas",
+      color:'brad-bg-overlay-60'
     });
-    console.log(this.popoverService);
+    console.log(this.popoverService)
   }
 
   list = [
     {
-      id: '1',
-      text: 'Utilizando Span <span class="brad-font-weight-bold brad-font-paragraph-sm brad-text-color-neutral-0"> Favoritos </span>como quiser. Quer ver como fazer?',
-      target: 'firstStepTutorial',
+      id: "1",
+      text: "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices venenatis diam a commodo. Maecenas nulla arcu, auctor aliquam mauris non, vehicula eleifend nisl.",
+      target: "target-345",
     },
     {
-      id: '2',
-      text: 'Utilizando EM sem aria<em class="brad-font-weight-bold brad-font-paragraph-sm brad-text-color-neutral-0"> Favoritos </em>como quiser. Quer ver como fazer?',
-      target: 'firstStepTutorial',
+      id: "2",
+      text: "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices venenatis diam a commodo. Maecenas nulla arcu, auctor aliquam mauris non, vehicula eleifend nisl.",
+      target: "target-2",
     },
     {
-      id: '3',
-      text: 'Utilizando EM com aria-label <em aria-label="Favoritos" class="brad-font-weight-bold brad-font-paragraph-sm brad-text-color-neutral-0"> Favoritos </em>como quiser. Quer ver como fazer?',
-      target: 'firstStepTutorial',
-    },
-    {
-      id: '4',
-      text: 'Utilizando EM e aria-label hide <em aria-hidden="true" class="brad-font-weight-bold brad-font-paragraph-sm brad-text-color-neutral-0">Favoritos</em><span aria-hidden="true" class="brad-font-weight-bold brad-font-paragraph-sm brad-text-color-neutral-0" style="opacity: 0;">Favoritos</span>como quiser. Quer ver como fazer?',
-      target: 'cardSalvar',
+      id: "3",
+      text: "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices venenatis diam a commodo. Maecenas nulla arcu, auctor aliquam mauris non, vehicula eleifend nisl.",
+      target: "target-3",
     },
   ];
 
-  closeTutorial() {
+   closeTutorial() {
     this.overlayService.close();
     this.popoverService.close();
   }
-  openTutorial(idTarget: any, index: number) {
-    this.popoverService = LiquidCorp.BradPopoverService.getInstance({
-      id: 'popover-151',
-      idTarget: 'target-345',
-      list: this.list,
-      currentItem: this.list[0],
-      direction: 'bottom',
-    });
-
-    this.overlayService = LiquidCorp.BradOverlayService.getInstance({
-      id: 'myCanvas',
-      color: 'brad-bg-overlay-60',
-    });
+   openTutorial(idTarget:any) {
     this.popoverService.open();
     this.popoverService.resetStepper(idTarget);
     this.overlayService.open(idTarget);
   }
-  prevTutorial() {
+   prevTutorial() {
     this.popoverService.prevStepper();
-    this.overlayService.updateTarget(
-      this.popoverService.stepper.currentItem.target
-    );
+    this.overlayService.updateTarget(this.popoverService.stepper.currentItem.target);
   }
-  nextTutorial() {
+   nextTutorial() {
     this.popoverService.nextStepper();
-    this.overlayService.updateTarget(
-      this.popoverService.stepper.currentItem.target
-    );
+    this.overlayService.updateTarget(this.popoverService.stepper.currentItem.target);
   }
 
-  onSelectionChange(selectedId: string) {
-    console.log('itrem', selectedId);
-    const firstValueItem = this.list[0];
-    const receivedValueItem = this.list.findIndex((x) => x.id === selectedId);
-    if (receivedValueItem) {
-      this.list[0] = this.list[receivedValueItem];
-      this.list[receivedValueItem] = firstValueItem;
-    }
-    console.log('teste', this.list);
-  }
 }
