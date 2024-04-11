a3613079b97a902989b1bfb30bdb765fdd6aba39
Victor Oliveira <victor.oliveira@avanade.com>
2024-04-11 16:07:25 -0300

    fix: melhroia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TestComponent } from './test/test.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { PopoverStepperComponent } from './components/popover-stepper/popover-stepper/popover-stepper.component';
 import { FormsModule } from '@angular/forms';
 
@@ -13,7 +12,6 @@ import { FormsModule } from '@angular/forms';
   declarations: [
     AppComponent,
     TestComponent,
-    PagenotfoundComponent,
     PopoverStepperComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
